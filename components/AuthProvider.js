d57609ef89c75bfa24884b0d23e0e74fd4e549e6
Prince clumson-eklu <ameto888@protonmail.com>
2025-01-16 15:48:07 -0500

    Create Login and Logout functiond.

--- components/AuthProvider.js
+++ components/AuthProvider.js
@@ -7,6 +7,14 @@ export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
+  const login = (useData) => {
+    setUser(userData);
+  }
+
+  const logout= () => {
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider value={{ user}}>
       {children}
